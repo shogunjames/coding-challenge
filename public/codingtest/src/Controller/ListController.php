614fdb9a2e56324947b4d5b3bb664e32f21a5a7b
Roshny <roshny.j@gmail.com>
2019-04-05 10:32:23 +0530

    added changed  files to git

--- public/codingtest/src/Controller/ListController.php
+++ public/codingtest/src/Controller/ListController.php
@@ -23,105 +23,128 @@ class ListController extends Controller
 
         $rootPath = $this->container->get('kernel')->getRootDir();
         $filePath = $rootPath . '/../data/json/jbl-ulse-3-im-test_104454.json';
+        //$filePath = $rootPath . '/../data/json/teufel-real-blue-im-test_104508.json';
         //echo $filePath;
         $contentvar = file_get_contents($filePath);
-        dump($contentvar);
+        /*
+        $collectionConstraint = new Assert\Collection(
+        array(
+        'headline' => new Assert\Type(array('type'=>'integer')),
+        'urlid' => new Assert\Type(array('type'=>'string')),
+        'value' => new Assert\Type(array('type'=>'double'))
+        )
+        ); */
+
+        //dump($contentvar);die;
         $data = json_decode($contentvar, true);
         dump($data);
-        //die;
 
-        try {
-            $entityManager = $this->getDoctrine()->getManager();
-            //$entityManager->getRepository('Blog::class')->findOneByName($data['urlId']);
+        if ($data) {
 
-            //if (($blog = $entityManager->getRepository('Blog::class')->findOneByName($data['urlId']))==null)
-        //{
-                
-                $blog = new Blog();
 
-                $blog->setHeadline($data['headline']);
-                $blog->setUrlId($data['urlId']);
-                $blog->setUrlslug($data['urlSlug']);
+            //die;
 
-                $blog->setSubtitle($data['subtitle']);
-                $blog->setIntroductiontext($data['introduction']);
+            try {
+                $entityManager = $this->getDoctrine()->getManager();
+                //$entityManager->getRepository('Blog::class')->findOneByName($data['urlId']);
 
-                $blog->setDisplayDate($data['displayDate']['timestamp']);
+                //if (($blog = $entityManager->getRepository('Blog::class')->findOneByName($data['urlId']))==null)
+                //{
+                $checkexists = $this->getDoctrine()->getRepository(Blog::class)->findBy(array('urlid' => $data['urlId']));
+                //dump($checkexists);
+                if (empty($checkexists)) {
+                    $blog = new Blog();
 
-                $blog->setAuthorid($data['author']['id']);
+                    $blog->setHeadline($data['headline']);
+                    $blog->setUrlId($data['urlId']);
+                    $blog->setUrlslug($data['urlSlug']);
 
-                $blog->setAuthorfirstname($data['author']['firstName']);
+                    $blog->setSubtitle($data['subtitle']);
+                    $blog->setIntroductiontext($data['introduction']);
 
-                $blog->setAuthorlastname($data['author']['lastName']);
+                    $blog->setDisplayDate($data['displayDate']['timestamp']);
 
-                $blog->setImageid($data['image']['id']);
+                    $blog->setAuthorid($data['author']['id']);
 
-                $blog->setHeight($data['image']['height']);
+                    $blog->setAuthorfirstname($data['author']['firstName']);
 
-                $blog->setWidth($data['image']['width']);
+                    $blog->setAuthorlastname($data['author']['lastName']);
 
-                $blog->setImagetext($data['image']['text']);
+                    $blog->setImageid($data['image']['id']);
 
-                $blog->setUrl($data['image']['url']);
+                    $blog->setHeight($data['image']['height']);
 
-                $blog->setSource($data['image']['source']);
+                    $blog->setWidth($data['image']['width']);
 
-                $entityManager->persist($blog);
+                    $blog->setImagetext($data['image']['text']);
 
-                $entityManager->flush();
+                    $blog->setUrl($data['image']['url']);
+
+                    $blog->setSource($data['image']['source']);
+
+                    $entityManager->persist($blog);
+
+                    $entityManager->flush();
                 }
-           // }
-        catch (UniqueConstraintViolationException $e) {
+            } catch (UniqueConstraintViolationException $e) {
                 $this->getDoctrine()->resetManager();
-          }
-        
-
-        $lastinsertedId = $blog->getId();
-
-
-
-        $chapt = $data['chapters'];
-        dump($data['chapters']);
-        // die;
-
-        foreach ($chapt as $chapter) {
-            $chapters = new Lesson();
-            $chapters->setChaptersorder($chapter['id']);
-
-            $chapters->setBlogkey($lastinsertedId);
-
-            $chapters->setChaptersheadline($chapter['headline']);
-
-            $chapters->setChapterstext($chapter['text']);
-            if ($chapter['image']) {
-
-                $chapters->setChapterimageid($chapter['image']['id']);
-
-                $chapters->setChapterimageheight($chapter['image']['height']);
-
-                $chapters->setChapterimagewidth($chapter['image']['width']);
-
-                $chapters->setChapterimagetext($chapter['image']['text']);
-
-                $chapters->setChapterimageurl($chapter['image']['url']);
-
-                $chapters->setChapterimagesource($chapter['image']['source']);
-
             }
 
-            //dump($chapters);
+            if (empty($checkexists)) {
+                $lastinsertedId = $blog->getId();
 
-            dump($chapter['image']);
+                $checkexistsLesson = $this->getDoctrine()->getRepository(Lesson::class)->findBy(array('blogkey' => $lastinsertedId));
+                //dump($checkexists);
+                if (empty($checkexistsLesson)) {
 
-            $entityManager->persist($chapters);
-            $entityManager->flush();
+                    $chapt = $data['chapters'];
+                    dump($data['chapters']);
+                    // die;
 
+                    foreach ($chapt as $chapter) {
+                        $chapters = new Lesson();
+                        $chapters->setChaptersorder($chapter['id']);
+
+                        $chapters->setBlogkey($lastinsertedId);
+
+                        $chapters->setChaptersheadline($chapter['headline']);
+
+                        $chapters->setChapterstext($chapter['text']);
+                        if ($chapter['image']) {
+
+                            $chapters->setChapterimageid($chapter['image']['id']);
+
+                            $chapters->setChapterimageheight($chapter['image']['height']);
+
+                            $chapters->setChapterimagewidth($chapter['image']['width']);
+
+                            $chapters->setChapterimagetext($chapter['image']['text']);
+
+                            $chapters->setChapterimageurl($chapter['image']['url']);
+
+                            $chapters->setChapterimagesource($chapter['image']['source']);
+
+                        }
+
+                        //dump($chapters);
+
+                        dump($chapter['image']);
+
+                        $entityManager->persist($chapters);
+                        $entityManager->flush();
+
+                    }
+                }
+            }
         }
-
         //return new Response('Saved an blog with the id of  '.$blog->getId());
 
         $blogs = $this->getDoctrine()->getRepository(Blog::class)->findAll(array('distinct' => true));
-        return $this->render('blogs/index.html.twig', array('articles' => $blogs));
+        if ($data) {
+            return $this->render('blogs/index.html.twig', array('articles' => $blogs));
+        } else {
+            return $this->render('blogs/index.html.twig', array('articles' => ''));
+        }
 
     }
 
@@ -148,12 +171,12 @@ class ListController extends Controller
      * @Route("/chapters/{id}",name="chapters_show")
      */
     /* public function show($id)
-    {
-     // $entityManager = $this->getDoctrine()->getManager();
-        $blog1 = $this->getDoctrine()->getRepository(Chapters::class)->find($id);
-        //dump($blog1);die;
-        return $this->render('blogs/show.html.twig', array('blog' => $blog1));
+{
+// $entityManager = $this->getDoctrine()->getManager();
+$blog1 = $this->getDoctrine()->getRepository(Chapters::class)->find($id);
+//dump($blog1);die;
+return $this->render('blogs/show.html.twig', array('blog' => $blog1));
 
-    }
+}
  */
 }
